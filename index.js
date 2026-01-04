848714a124ad5a4c5e491feb13156bb60054abc1
Atharva_Shimpi <atharvashimpi2005@gmail.com>
2026-01-05 02:58:59 +0530

    Update filename in gist to include ellipsis
    Changed the Empty Bar design inside generateBarChart function and updated the filename.

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ async function updateLeetCodeGist(leetcode) {
             gist_id: gist_id,
             files: {
                 [filename]: {
-                    filename: `💻 My LeetCode Stats ✨`,
+                    filename: `💻 My LeetCode Stats...`,
                     content: lines.join("\n")
                 }
             }
@@ -77,7 +77,7 @@ function generateBarChart(percent, size) {
 
     return (
         "█".repeat(filled) +
-        " ".repeat(empty)
+        "░".repeat(empty)
     );
 }
 
